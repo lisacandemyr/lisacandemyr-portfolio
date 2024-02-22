11beb324bf13eb6423e9fe24aba06b5e0e996acd
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2024-02-22 12:31:49 +0000

    cleaned up code for project component

--- src/main/ProjectPage.js
+++ src/main/ProjectPage.js
@@ -4,69 +4,64 @@ function ProjectPage({ toggleDarkMode }) {
   return (
     <div className="ProjectPage hidden page" id="project-page">
       <div className="project-page">
-        <div className="row">
-          <div className="col-6"></div>
-          <div className="col-4">
-            <ul className="float-left">
-              <li
-                className={`${
-                  toggleDarkMode ? "project-light-mode" : "project-dark-mode"
-                }`}
-              >
-                01
-                <button
-                  className={`${
-                    toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
-                  }`}
-                >
-                  template
-                </button>
-              </li>
-              <li
-                className={`${
-                  toggleDarkMode ? "project-light-mode" : "project-dark-mode"
-                }`}
-              >
-                02
-                <button
-                  className={`${
-                    toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
-                  }`}
-                >
-                  template
-                </button>
-              </li>
-              <li
-                className={`${
-                  toggleDarkMode ? "project-light-mode" : "project-dark-mode"
-                }`}
-              >
-                03
-                <button
-                  className={`${
-                    toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
-                  }`}
-                >
-                  template
-                </button>
-              </li>
-              <li
-                className={`${
-                  toggleDarkMode ? "project-light-mode" : "project-dark-mode"
-                }`}
-              >
-                04
-                <button
-                  className={`${
-                    toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
-                  }`}
-                >
-                  template
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <ul className="float-left">
+          <li
+            className={`${
+              toggleDarkMode ? "project-light-mode" : "project-dark-mode"
+            }`}
+          >
+            01
+            <button
+              className={`${
+                toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
+              }`}
+            >
+              Project
+            </button>
+          </li>
+          <li
+            className={`${
+              toggleDarkMode ? "project-light-mode" : "project-dark-mode"
+            }`}
+          >
+            02
+            <button
+              className={`${
+                toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
+              }`}
+            >
+              Project
+            </button>
+          </li>
+          <li
+            className={`${
+              toggleDarkMode ? "project-light-mode" : "project-dark-mode"
+            }`}
+          >
+            03
+            <button
+              className={`${
+                toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
+              }`}
+            >
+              Project
+            </button>
+          </li>
+          <li
+            className={`${
+              toggleDarkMode ? "project-light-mode" : "project-dark-mode"
+            }`}
+          >
+            04
+            <button
+              className={`${
+                toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
+              }`}
+            >
+              Project
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
