c88e8a4500c7c62d3a4b05ff88f4c4ba09ed4706
Lisa Candemyr <lisa_candemyr@hotmail.com>
2024-06-21 12:41:38 +0100

    added projectTwo

---
+++ src/main/projects/ProjectTwo.js
@@ -0,0 +1,155 @@
+import React from "react";
+import "../ProjectPage.css";
+import ProjectCardTwo from "./project-card-two.png";
+
+function ProjectTwo({ toggleDarkMode }) {
+  const onClickPrev = (currentProjectId, prevProjectId) => {
+    const currentProject = document.getElementById(currentProjectId);
+    const prevProject = document.getElementById(prevProjectId);
+  
+    if (!prevProject || !currentProject) {
+      return; // Handle case where elements are not found
+    }
+  
+    if (!prevProject.classList.contains("hidden")) {
+      return; // If previous project is already visible, do nothing
+    }
+  
+    currentProject.classList.add("fade-out");
+    setTimeout(() => {
+      currentProject.classList.add("hidden");
+      currentProject.classList.remove("fade-out");
+      prevProject.classList.remove("hidden");
+    }, 1000);
+  };
+
+  return (
+    <div className="ProjectTwo hidden" id="project-2">
+      <div className="grid">
+      <div>
+          <img src={ProjectCardTwo} className={`project-grid-item-2-1 float-left ${
+          toggleDarkMode ? "img-light-mode" : "img-dark-mode"
+        }`} alt="The Ace of Pentacles Tarot Card" />
+        </div>
+        <div
+          className={`project-grid-item-2-2 float-right ${
+            toggleDarkMode ? "grid-2-light-mode" : "grid-2-dark-mode"
+          }`}
+        >
+          <div
+            className={`row ${
+              toggleDarkMode ? "text-light-mode" : "text-dark-mode"
+            }`}
+          >
+            <div
+              className={`col-lg-4 ${
+                toggleDarkMode ? "col-light-mode" : "col-dark-mode"
+              }`}
+            >
+              <ul>
+                <li>
+                  Code
+                  <br />
+                  <span>Tools and Hosting Platforms</span>
+                </li>
+                <p className="tool-specifications">
+                  VS Code - GitHub - Netlify.
+                </p>
+                <br />
+                <li>
+                  Front-end Languages
+                  <br />
+                  <span>Libraries and Frameworks</span>
+                </li>
+                <p className="tool-specifications">
+                  JavaScript - React - Bootstrap.
+                </p>
+                <br />
+                <li>
+                  Design
+                  <br />
+                  <span>Tools</span>
+                </li>
+                <p className="tool-specifications"><a
+                  title="SheCodes"
+                  href="https://icons8.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={`project-link ${
+                    toggleDarkMode ? "text-light-mode" : "text-dark-mode"
+                  }`}>Icons8</a>.</p>
+                <br />
+                <li>
+                  Photo
+                  <br />
+                  <span>Editing Tools</span>
+                </li>
+                <p className="tool-specifications">N/A.</p>
+              </ul>
+            </div>
+            <div className="col-lg-8">
+              <p>
+                My second project is a mock-up page designed for a company specialising in polyurethane insulation materials.
+                <br />
+                <br />
+                <em>
+                The icon chosen for the header is inspired by what I envision polyurethane looks like under a microscope, enhanced with playful colours to make it more engaging. The subsequent icons are chosen to complement the first one, creating a cohesive and continuous aesthetic throughout the project. Once again, I've opted for a static page design, but this time with scrollable content. The colour palette has been updated to a more technological look, with elements styled using glassmorphism to truly capture the modern feel, departing from the company's traditional white and blue scheme. To add a bit of fun distinct from my previous project, I've added a hover glitch effect to the headline and implemented a more interactive menu.
+                </em>
+              </p>
+              <a
+                title="Lisa's Putechnology Project"
+                href="https://putechnology.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+                className={`link btn ${
+                  toggleDarkMode ? "links-light-mode" : "links-dark-mode"
+                }`}
+              >
+                Show Project
+              </a>
+              <a
+                title="Link to Lisa's GitHub for Portfolio Project"
+                href="https://github.com/lisacandemyr/putechnology"
+                target="_blank"
+                rel="noreferrer"
+                className={`link btn ${
+                  toggleDarkMode ? "links-light-mode" : "links-dark-mode"
+                }`}
+              >
+                GitHub Code
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="btn-container text-end">
+      <button className="project-prev-btn btn"
+        title="Previous Project - Lisa's Portfolio"
+        onClick={() => onClickPrev("project-2", "project-1")}
+        >
+          <i
+            className={`fa-thin fa-angle-left ${
+              toggleDarkMode
+                ? "project-btn-light-mode"
+                : "project-btn-dark-mode"
+            }`}
+          ></i>
+        </button>
+        <button className="project-next-btn btn"
+        title="Next Project - Pending.."
+        disabled
+        >
+          <i
+            className={`fa-thin fa-angle-right ${
+              toggleDarkMode
+                ? "project-btn-light-mode"
+                : "project-btn-dark-mode"
+            }`}
+          ></i>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectTwo;
--- src/main/projects/Projects.js
+++ src/main/projects/Projects.js
@@ -1,38 +1,98 @@
-import logoLight from "../logo-light.png";
-import logoDark from "../logo-dark.png";
+import React from 'react';
+import logoLightMode from "../logo-light-mode.png";
+import logoDarkMode from "../logo-dark-mode.png";
 import "../ProjectPage.css";
 
 function Projects({ toggleDarkMode }) {
   // Function to show select project page
-  const handleClick = () => {
+  const handleClick = (projectId) => {
     const projectPage = document.getElementById("project-page");
     const project1 = document.getElementById("project-1");
+    const project2 = document.getElementById("project-2");
+    const project3 = document.getElementById("project-3");
+    const project4 = document.getElementById("project-4");
 
-    if (project1.classList.contains("hidden")) {
-      projectPage.classList.add("fade-out");
-      setTimeout(() => {
-        projectPage.classList.add("hidden");
-        projectPage.classList.remove("fade-out");
-      }, 1000);
-      setTimeout(() => {
-        project1.classList.remove("hidden");
-      }, 1100);
-    } else {
-      project1.classList.add("fade-out");
-      setTimeout(() => {
-        project1.classList.add("hidden");
-        project1.classList.remove("fade-out");
-      }, 1000);
-      setTimeout(() => {
-        projectPage.classList.remove("hidden");
-      }, 1100);
+    const hideAllProjects = () => {
+      project1.classList.add("hidden");
+      project2.classList.add("hidden");
+    };
+
+    if (projectId === "project-1") {
+      if (project1.classList.contains("hidden")) {
+        projectPage.classList.add("fade-out");
+        setTimeout(() => {
+          projectPage.classList.add("hidden");
+          projectPage.classList.remove("fade-out");
+          hideAllProjects();
+          project1.classList.remove("hidden");
+        }, 1000);
+      } else {
+        project1.classList.add("fade-out");
+        setTimeout(() => {
+          project1.classList.add("hidden");
+          project1.classList.remove("fade-out");
+          projectPage.classList.remove("hidden");
+        }, 1000);
+      }
+    } else if (projectId === "project-2") {
+      if (project2.classList.contains("hidden")) {
+        projectPage.classList.add("fade-out");
+        setTimeout(() => {
+          projectPage.classList.add("hidden");
+          projectPage.classList.remove("fade-out");
+          hideAllProjects();
+          project2.classList.remove("hidden");
+        }, 1000);
+      } else {
+        project2.classList.add("fade-out");
+        setTimeout(() => {
+          project2.classList.add("hidden");
+          project2.classList.remove("fade-out");
+          projectPage.classList.remove("hidden");
+        }, 1000);
+      }
+    } else if (projectId === "project-3") {
+      if (project3.classList.contains("hidden")) {
+        projectPage.classList.add("fade-out");
+        setTimeout(() => {
+          projectPage.classList.add("hidden");
+          projectPage.classList.remove("fade-out");
+          hideAllProjects();
+          project3.classList.remove("hidden");
+        }, 1000);
+      } else {
+        project3.classList.add("fade-out");
+        setTimeout(() => {
+          project3.classList.add("hidden");
+          project3.classList.remove("fade-out");
+          projectPage.classList.remove("hidden");
+        }, 1000);
+      }
+    } else if (projectId === "project-4") {
+      if (project4.classList.contains("hidden")) {
+        projectPage.classList.add("fade-out");
+        setTimeout(() => {
+          projectPage.classList.add("hidden");
+          projectPage.classList.remove("fade-out");
+          hideAllProjects();
+          project4.classList.remove("hidden");
+        }, 1000);
+      } else {
+        project4.classList.add("fade-out");
+        setTimeout(() => {
+          project4.classList.add("hidden");
+          project4.classList.remove("fade-out");
+          projectPage.classList.remove("hidden");
+        }, 1000);
+      }
     }
   };
 
   return (
     <div className="Projects hidden" id="project-page">
       <ul className="float-left">
-        <li>
+        <li className={`${toggleDarkMode ? "text-light-mode" : "text-dark-mode"}`}
+        >
           01
           <button
             title="Portfolio Project"
@@ -42,7 +102,7 @@ function Projects({ toggleDarkMode }) {
             }`}
           >
             <img
-              src={`${toggleDarkMode ? logoLight : logoDark}`}
+              src={`${toggleDarkMode ? logoLightMode : logoDarkMode}`}
               className="logo"
               height="45"
               width="45"
@@ -51,21 +111,25 @@ function Projects({ toggleDarkMode }) {
             portfolio
           </button>
         </li>
-        <li>
+        <li className={`${toggleDarkMode ? "text-light-mode" : "text-dark-mode"}`}
+        >
           02
           <button
-            title="Project Pending.."
+            title="Putechnology Project"
+            onClick={() => handleClick("project-2")}
             className={`ms-2 ${
               toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
             }`}
           >
-            Pending..
+            Putechnology
           </button>
         </li>
-        <li>
+        <li className={`${toggleDarkMode ? "text-light-mode" : "text-dark-mode"}`}
+        >
           03
           <button
             title="Project Pending.."
+            onClick={() => handleClick("project-3")}
             className={`ms-2 ${
               toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
             }`}
@@ -73,10 +137,12 @@ function Projects({ toggleDarkMode }) {
             Pending..
           </button>
         </li>
-        <li>
+        <li className={`${toggleDarkMode ? "text-light-mode" : "text-dark-mode"}`}
+        >
           04
           <button
             title="Project Pending.."
+            onClick={() => handleClick("project-4")}
             className={`ms-2 ${
               toggleDarkMode ? "outline-light-mode" : "outline-dark-mode"
             }`}
